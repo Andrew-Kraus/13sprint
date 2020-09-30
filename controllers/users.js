6cbccf67d0cb930473d71e822cc104d795feff7d
Andrew Kraus <KalkalovichIvan@yandex.ru>
2020-09-30 16:30:46 +0300

    v0.0.5

--- controllers/users.js
+++ controllers/users.js
@@ -22,5 +22,5 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(400).send(err));
+    .catch(() => res.status(400).send({ message: 'Ошибка' }));
 };
